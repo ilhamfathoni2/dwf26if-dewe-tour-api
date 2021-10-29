60f14e274a5616a55f6292b0f865c52baba9209b
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-10-29 19:25:24 +0700

    Make models user, country, trip, transaction

---
+++ migrations/20211029122415-create-trip.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('trips', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.INTEGER
+      },
+      accomodation: {
+        type: Sequelize.STRING
+      },
+      transportation: {
+        type: Sequelize.STRING
+      },
+      eat: {
+        type: Sequelize.STRING
+      },
+      day: {
+        type: Sequelize.STRING
+      },
+      night: {
+        type: Sequelize.STRING
+      },
+      dateTrip: {
+        type: Sequelize.DATE
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      quota: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('trips');
+  }
+};
---
+++ models/transaction.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  transaction.init({
+    id: DataTypes.INTEGER,
+    counterQty: DataTypes.INTEGER,
+    total: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    attachment: DataTypes.STRING,
+    tripId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'transaction',
+  });
+  return transaction;
+};
