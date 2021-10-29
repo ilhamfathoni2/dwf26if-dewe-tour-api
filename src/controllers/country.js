b4be03b7fa78338d287f85ead98533ccbc05d4ed
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-10-29 21:41:29 +0700

    Insert Country

--- src/controllers/country.js
+++ src/controllers/country.js
@@ -3,9 +3,15 @@ const { country } = require("../../models");
 exports.addCountrys = async (req, res) => {
   try {
     await country.create(req.body);
+    const data = await country.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
     res.send({
       status: "success",
       message: "Add country success",
+      datas: data,
     });
   } catch (error) {
     console.log(error);
