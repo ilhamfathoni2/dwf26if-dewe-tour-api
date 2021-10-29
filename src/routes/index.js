7f71a668e0a9546d35e5b428c51d0a41c8ddc380
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-10-30 05:41:33 +0700

    Get all data country and data match params, and delete data

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { addCountrys, getCountry } = require("../controllers/country");
+const {
+  addCountrys,
+  getCountry,
+  getAllCountry,
+  deleteCountry,
+} = require("../controllers/country");
 
 // Route
 router.post("/country", addCountrys);
+router.get("/country", getAllCountry);
 router.get("/country/:id", getCountry);
+router.delete("/country/:id", deleteCountry);
 
 module.exports = router;
