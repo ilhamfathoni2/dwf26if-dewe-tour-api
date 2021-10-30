37e2689b92543c1867ae7a81859b3964c54424ea
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-10-30 11:18:15 +0700

    Relation

--- models/trip.js
+++ models/trip.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class trip extends Model {
     /**
@@ -11,24 +9,32 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      trip.belongsTo(models.country, {
+        foreignKey: {
+          name: "countryId",
+        },
+      });
     }
-  };
-  trip.init({
-    title: DataTypes.STRING,
-    country: DataTypes.INTEGER,
-    accomodation: DataTypes.STRING,
-    transportation: DataTypes.STRING,
-    eat: DataTypes.STRING,
-    day: DataTypes.STRING,
-    night: DataTypes.STRING,
-    dateTrip: DataTypes.DATE,
-    price: DataTypes.INTEGER,
-    quota: DataTypes.STRING,
-    description: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'trip',
-  });
+  }
+  trip.init(
+    {
+      title: DataTypes.STRING,
+      countryId: DataTypes.INTEGER,
+      accomodation: DataTypes.STRING,
+      transportation: DataTypes.STRING,
+      eat: DataTypes.STRING,
+      day: DataTypes.STRING,
+      night: DataTypes.STRING,
+      dateTrip: DataTypes.DATE,
+      price: DataTypes.INTEGER,
+      quota: DataTypes.STRING,
+      description: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "trip",
+    }
+  );
   return trip;
 };
